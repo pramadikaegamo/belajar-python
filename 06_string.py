c0ca355a284ef0f83bc423061d95fa129aeddd62
Pramadika Egamo <dikaegamo1@gmail.com>
2023-11-20 14:02:47 +0700

    Create 06_string.py

---
+++ 06_string.py
@@ -0,0 +1,11 @@
+# Belajar String
+
+nama = "Pramadika Egamo"
+kota = 'Yogyakarta'
+alamat = 'Seleman'
+
+nama_depan = "Pramadika"
+nama_belakang = "Egamo"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+print(nama_lengkap)
