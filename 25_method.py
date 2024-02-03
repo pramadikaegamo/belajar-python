ace6a4c594e1a8159d62296a52fa37b6522f5209
Pramadika Egamo <dikaegamo1@gmail.com>
2024-02-03 22:25:57 +0700

    Create 25_method.py

---
+++ 25_method.py
@@ -0,0 +1,19 @@
+# belajr membuat method / function
+
+nama = []
+
+
+def print_nama():
+    print("================")
+    for i in nama:
+        print(i)
+
+
+nama.append("eko")
+print_nama()
+
+nama.append("kurniawan")
+print_nama()
+
+nama.append("egamo")
+print_nama()
