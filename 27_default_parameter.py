a39c0f91f1bb3c908c09969e191d07496d27c3c7
Pramadika Egamo <dikaegamo1@gmail.com>
2024-02-03 22:26:03 +0700

    Create 27_default_parameter.py

---
+++ 27_default_parameter.py
@@ -0,0 +1,9 @@
+# belajar default argument value
+
+
+def say_hello(first_name="ujang", last_name=""):
+    print(f"Hello {first_name} - {last_name}")
+
+
+say_hello("dika")
+say_hello(last_name="egamo", first_name="ikin")
