f34f8cb499391fec63ea6f4d2be7dfd02aaec7d9
Pramadika Egamo <dikaegamo1@gmail.com>
2024-02-04 17:14:00 +0700

    Update main.py

--- oop python/main.py
+++ oop python/main.py
@@ -3,25 +3,46 @@
 
 
 class player:
-    def __init__(self, health=100, energy=100):
+    def __init__(self, name, health=100, energy=100):
+        self.name = name
         self.health = health
         self.energy = energy
 
     def attack(self, target, damage=1):
         target.health -= damage
         self.energy -= damage
-        print(f"attacking to monster, monster health : {target.health} left")
+        print(f"{self.name} attacking {damage} damage to {target.name}")
+        if target.is_attacked(palyer_name=self.name):
+            self.health -= target.damage
+
+    def show_info(self):
+        print(f"{self.name} energy : {self.energy}")
+        print(f"{self.name} health : {self.health}")
 
 
 class monster:
-    def __init__(self, health=500):
-        self.health = health
+    def __init__(self, name, health=100):
+        self.name = name
+        self.health = health  # dinamis
+        self.health_init = self.health  # 500
+        self.damage = 10
+
+    def is_attacked(self, palyer_name):
+        print(f"{self.name} attack {self.damage} damage to {palyer_name}")
+        return self.health < self.health_init
+
+    def show_info(self):
+        print(f"{self.name} health : {self.health}")
 
 
-player1 = player()
-player2 = player()
-dragon = monster(10000)
-
+player1 = player(name="balmond")
+player2 = player(name="layla")
+dragon = monster(name="dragon", health=500)
+tiger = monster(name="tiger")
 
 player1.attack(target=dragon, damage=80)
-player2.attack(target=dragon)
+player2.attack(target=tiger, damage=20)
+
+player1.show_info()
+player2.show_info()
+# dragon.show_info()
